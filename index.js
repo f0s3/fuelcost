2f963edd59cbb102f7256f4bf5ba61c570c7ba55
Andrii Kosenko <andriikos@softwareplanetgroup.com>
2022-06-18 22:06:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import TelegramBot from 'node-telegram-bot-api';
+
+const token = process.env.TG_KEY;
+
+const bot = new TelegramBot(token, { polling: true })
+
+const fueltextCommandRegex = /\/fuelcost\s+(\d+(?:\.|\,)\d+|\d+)(?:\.|\,)?\s+(\d+(?:\.|\,)\d+|\d+)(?:\.|\,)?\s+(\d+(?:\.|\,)\d+|\d+)(?:\.|\,)?/g;
+
+bot.onText(fueltextCommandRegex, async (message, match) => {
+  const prepareNumber = (n) => Number(n.replaceAll(',', '.'));
+  const moneyAmount = ((prepareNumber(match[1]) / 100) * prepareNumber(match[2])) * prepareNumber(match[3]);
+
+  bot.sendMessage(message.chat.id, `Amount of money for ${prepareNumber(match[2])} km is: ${moneyAmount}`);
+});
+
+bot.onText(/\/help/, async (msg) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId,
+    'Guide:\n' +
+    '          {fuel consumption per 100km} {km driven} {fuel cost}\n' +
+    '/fuelcost             9.7                 13.5        50.4\n' +
+    '\n'
+  )
+});
+
+console.log('Bot started');
+
